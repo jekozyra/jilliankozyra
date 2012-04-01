4b41def538c1fca00f5171d77eaf28dcdf798f62
Jillian <Jillyh0@gmail.com>
2012-04-02 00:44:28 +0200

    updating user stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  attr_accessible :name, :party, :email, :password, :password_confirmation
+  attr_accessible :name, :party, :email, :password, :password_confirmation, :admin
   validates_presence_of :password, :on => :create
   
   def admin?
