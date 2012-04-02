2b9d0ab38145f0057dc54086f43f5ce2d0f20141
Jillian <Jillyh0@gmail.com>
2012-04-02 04:05:58 +0200

    updating participants stuff

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -1,6 +1,6 @@
 class AdministrationController < ApplicationController
   
-  before_filter :admin_authorize
+  #before_filter :admin_authorize
   
   layout 'survey'
   
@@ -23,5 +23,58 @@ class AdministrationController < ApplicationController
   
   def submit_notify_participants
   end
+  
+  
+  def load_records
+    
+    if File.exists?("#{Rails.public_path}/files/rada_ukr") and File.exists?("#{Rails.public_path}/files/rada_eng")
+      IO.foreach("#{Rails.public_path}/files/rada_ukr") do |line|
+        rep_ukr = line.chomp.split("|")
+=begin
+        puts "Original Name: " + rep_ukr[0]
+        puts "Email: " + rep_ukr[1]
+        puts "Party: " + rep_ukr[2]
+        puts "List position: " + rep_ukr[3]
+        puts "Profile link: " + rep_ukr[4]
+=end    
+        unless Participant.exists?(:profile_link => rep_ukr[4])
+          participant = Participant.new(:original_name => rep_ukr[0],
+                                         :email => rep_ukr[1],
+                                         :party => rep_ukr[2],
+                                         :list_position => rep_ukr[3],
+                                         :profile_link => rep_ukr[4])
+        
+          participant.survey_code = rand(36**12).to_s(36)
+          if participant.save
+            puts "Created " + participant.original_name
+          else
+            puts "Failed to save " + participant.original_name
+          end
+        else
+          puts "Record already exists for " + rep_ukr[0]
+        end
+      end # done loading records from ukr file
+      
+      IO.foreach("#{Rails.public_path}/files/rada_eng") do |line|
+        rep_eng = line.chomp.split("|")
+        
+        participant = Participant.find_by_profile_link(rep_eng[2])
+        
+        participant.name = rep_eng[0] unless participant.nil?
+        participant.save! unless participant.nil?
+        
+        puts rep_eng[2] if participant.nil?
+        
+        #rep = name + "|" + email + "|" + profile_link
+        #puts rep_eng[0] ? "Name: " + rep_eng[0] : "------------------------------"
+        #puts rep_eng[1] ? "Email: " + rep_eng[1] : "------------------------------"
+        #puts rep_eng[2] ? "Profile link: " + rep_eng[2] : "------------------------------"
+      end
+      
+    end
+    
+    redirect_to participants_url
+    
+  end # end load_records
   
 end
