a6825ba1bbb52c2fc1a2dca08f04681e4e88e967
Jillian Kozyra <jilliankozyra@gmail.com>
2012-04-09 21:27:51 +0100

    changing email notification text

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,12 @@
+# encoding: UTF-8
+
 class NotificationMailer < ActionMailer::Base
   default from: "jilliankozyra@gmail.com"
   
   def survey_notifier(participant, survey)
     @participant = participant
     @survey = survey
-    mail(:to => @participant.email, :subject => "New survey - #{@survey.title}!")
+    mail(:to => @participant.email, :subject => "Опитування про коаліції у Верховній раді/Опрос о коалициях в Верховной раде/Survey on coalitions in the Verkhova Rada")
   end
   
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 class Participant < ActiveRecord::Base
   
   has_many :answers
@@ -6,10 +8,54 @@ class Participant < ActiveRecord::Base
   
   def both_names
     if self.name and self.original_name
-      "#{self.name}/#{self.original_name}"
+      "#{self.name}/#{self.original_name},"
     else
       self.name
     end
+  end
+  
+
+  def participant_greeting
+    
+    if self.original_name and self.name
+      if self.gender
+        if self.gender == "m"
+          "Шановний Депутат #{self.original_name.split(' ').first}/Уважаемый Депутат #{self.original_name.split(' ').first}/Dear Deputy #{self.name.split(' ').first},"
+        elsif self.gender == "f"
+          "Шановна Депутат #{self.original_name.split(' ').first}/Уважаемая Депутат #{self.original_name.split(' ').first}/Dear Deputy #{self.name.split(' ').first},"
+        else
+          "Шановні Депутат #{self.original_name.split(' ').first}/Уважаемые Депутат #{self.original_name.split(' ').first}/Dear Deputy #{self.name.split(' ').first},"
+        end
+      else
+        "Шановні Депутат #{self.original_name.split(' ').first}/Уважаемые Депутат #{self.original_name.split(' ').first}/Dear Deputy #{self.name.split(' ').first},"
+      end
+    else
+      if self.name
+        if self.gender
+          if self.gender == "m"
+            "Шановний Депутат #{self.name.split(' ').first}/Уважаемый Депутат #{self.name.split(' ').first}/Dear Deputy #{self.name.split(' ').first},"
+          elsif self.gender == "f"
+            "Шановна Депутат #{self.name.split(' ').first}/Уважаемая Депутат #{self.name.split(' ').first}/Dear Deputy #{self.name.split(' ').first},"
+          else
+            "Шановні Депутат #{self.name.split(' ').first}/Уважаемые Депутат #{self.name.split(' ').first}/Dear Deputy #{self.name.split(' ').first},"
+          end
+        else
+          "Шановні Депутат #{self.name.split(' ').first}/Уважаемые Депутат #{self.name.split(' ').first}/Dear Deputy #{self.name.split(' ').first},"
+        end
+      else
+        if self.gender
+          if self.gender == "m"
+            "Шановний Депутат #{self.original_name.split(' ').first}/Уважаемый Депутат #{self.original_name.split(' ').first}/Dear Deputy #{self.original_name.split(' ').first},"
+          elsif self.gender == "f"
+            "Шановна Депутат #{self.original_name.split(' ').first}/Уважаемая Депутат #{self.original_name.split(' ').first}/Dear Deputy #{self.original_name.split(' ').first},"
+          else
+            "Шановні Депутат #{self.original_name.split(' ').first}/Уважаемые Депутат #{self.original_name.split(' ').first}/Dear Deputy #{self.original_name.split(' ').first},"
+          end
+        else
+          "Шановні Депутат #{self.original_name.split(' ').first}/Уважаемые Депутат #{self.original_name.split(' ').first}/Dear Deputy #{self.original_name.split(' ').first},"
+        end
+      end
+    end  
   end
   
 end
