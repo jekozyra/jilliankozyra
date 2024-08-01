f0007786639563b438f7542df130ec53bc9be295
Jillian <jillian.kozyra@gmail.com>
2024-08-01 16:01:28 -0700

    feat: make a theme (#2)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,53 @@
 import type { Config } from "tailwindcss";
 
+const indices = [
+  "50",
+  "100",
+  "200",
+  "300",
+  "400",
+  "500",
+  "600",
+  "700",
+  "800",
+  "900",
+  "950",
+];
+
+const primary = indices.reduce<Record<string, string>>((accumulator, level) => {
+  accumulator[level] = `rgb(var(--base-${level}) / <alpha-value>)`;
+  accumulator[`${level}X`] = `rgb(var(--base-${level}X) / <alpha-value>)`;
+  return accumulator;
+}, {});
+
+const accent = indices.reduce<Record<string, string>>((accumulator, level) => {
+  accumulator[level] = `rgb(var(--accent-${level}) / <alpha-value>)`;
+  accumulator[`${level}X`] = `rgb(var(--accent-${level}X) / <alpha-value>)`;
+  return accumulator;
+}, {});
+
+const primaryDark = indices.reduce<Record<string, string>>(
+  (accumulator, level) => {
+    accumulator[level] = `rgb(var(--base-dark-${level}) / <alpha-value>)`;
+    accumulator[
+      `${level}X`
+    ] = `rgb(var(--base-dark-${level}X) / <alpha-value>)`;
+    return accumulator;
+  },
+  {}
+);
+
+const accentDark = indices.reduce<Record<string, string>>(
+  (accumulator, level) => {
+    accumulator[level] = `rgb(var(--accent-dark-${level}) / <alpha-value>)`;
+    accumulator[
+      `${level}X`
+    ] = `rgb(var(--accent-dark-${level}X) / <alpha-value>)`;
+    return accumulator;
+  },
+  {}
+);
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,10 +56,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-hanken-grotesk)", "sans-serif"],
+        serif: ["var(--font-martian-mono)", "serif"],
+        mono: ["var(--font-martian-mono)", "mono"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary,
+        accent,
+        primaryDark,
+        accentDark,
+        greenLight: "rgb(var(--green-light) / <alpha-value>)",
+        yellowDark: "rgb(var(--yellow-dark) / <alpha-value>)",
       },
     },
   },
